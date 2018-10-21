4c9dd00bc02335492fd3cf1db1a5a0fe254da9f3
Reno Sorensen <renojames19@gmail.com>
2018-10-21 23:12:22 +0000

    automatically backed up by learn

---
+++ my-cli-data-gem-project/lib/my/cli/data/gem/project.rb
@@ -0,0 +1,13 @@
+require "my/cli/data/gem/project/version"
+
+module My
+  module Cli
+    module Data
+      module Gem
+        module Project
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ my-cli-data-gem-project/lib/my/cli/data/gem/project/version.rb
@@ -0,0 +1,11 @@
+module My
+  module Cli
+    module Data
+      module Gem
+        module Project
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
